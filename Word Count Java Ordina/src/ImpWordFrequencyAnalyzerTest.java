05def690da2934d1e840172d95db55ea4f34b220
Chris van Dalen <78590845+ChrisvDalen@users.noreply.github.com>
2023-07-10 21:03:24 +0200

    Add files via upload

--- Word Count Java Ordina/src/ImpWordFrequencyAnalyzerTest.java
+++ Word Count Java Ordina/src/ImpWordFrequencyAnalyzerTest.java
@@ -11,6 +11,7 @@ public class ImpWordFrequencyAnalyzerTest {
         analyzer = new ImpWordFrequencyAnalyzer();
     }
 
+    // testCalculateHighestFrequency
     @Test
     public void testCalculateHighestFrequency() {
         String text = "As we set up our picnic, we noticed how beautifully the sun shines over the lake.";
@@ -18,13 +19,16 @@ public class ImpWordFrequencyAnalyzerTest {
         assertEquals(2, highestFrequency);
     }
 
+    // testCalculateFrequencyForWord
     @Test
     public void testCalculateFrequencyForWord() {
         String text = "As we set up our picnic, we noticed how beautifully the sun shines over the lake.";
         int frequency = analyzer.calculateFrequencyForWord(text, "the");
-        assertEquals(2, frequency);
+        assertEquals(3, frequency);
     }
 
+    
+    //  testCalculateMostFrequentNWords
     @Test
     public void testCalculateMostFrequentNWords() {
         String text = "As we set up our picnic, we noticed how beautifully the sun shines over the lake.";
@@ -33,4 +37,16 @@ public class ImpWordFrequencyAnalyzerTest {
         assertEquals("the", mostFrequentNWords.get(0).getWord());
         assertEquals(2, mostFrequentNWords.get(0).getFrequency());
     }
+    
+    // Test null input
+    
+    @Test
+    public void testCalculateFrequencyForWord_NullText() {
+        String text = null;
+        String word = "the";
+        int frequency = analyzer.calculateFrequencyForWord(text, word);
+        assertEquals(0, frequency, "Frequency for any word in a null text should be 0");
+    }
+
+
 }
