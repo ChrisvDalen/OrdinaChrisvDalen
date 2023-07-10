4dde5c5b980a0eeb2f55ab5e1dcf63a5f57264a8
Chris van Dalen <78590845+ChrisvDalen@users.noreply.github.com>
2023-07-10 21:03:59 +0200

    Delete Word Count Java Ordina directory

--- Word Count Java Ordina/src/WordFrequencyAnalyzer.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface WordFrequencyAnalyzer {
-
-	int calculateHighestFrequency(String text);
-	int calculateFrequencyForWord (String text, String word);
-	List<WordFrequency> calculateMostFrequentNWords (String text, int n);
-}
